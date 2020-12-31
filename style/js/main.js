d8415b93e06a798000d4d00dffe4618952f006e9
jsiddhesh105 <76804831+jsiddhesh105@users.noreply.github.com>
2020-12-31 12:15:54 +0530

    Phara rise work
    Submit

---
+++ style/js/main.js
@@ -0,0 +1,11 @@
+// const readmore = document.getElementById("read-more");
+// const ctext = document.querySelector('.ctext');
+
+// document.getElementById("read-more").addEventListener('click',function() {
+// 	// text.classList.toggle('show-more');
+// 	console.log("hellow");
+// })
+
+// document.getElementById("myBtn").addEventListener("click", function() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// });
